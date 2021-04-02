e53e2757ab825e1c31ac41690b48c77db6d0d68d
BunchDevelopment <brandon_bunch@icloud.com>
2021-04-01 20:27:33 -0500

    add server files

--- server/index.js
+++ server/index.js
@@ -1,28 +1,24 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const users = require('./users');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const mailingListController = require('./controllers/mailingList');
 
 const app = express();
-mongoose.connect('mongodb://localhost/NodePlayground');
+mongoose.connect('mongodb://localhost/test');
+
+app.use(express.json());
+app.use(cors());
+app.use('/mailingList', mailingListController);
+
 const db = mongoose.connection;
 
-let db_status = 'MongoDB connection not successful.';
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => (db_status = 'Successfully opened connection to Mongo!'));
+let db_status = 'Ey, i aint connected to the db';
 
-// middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api/users', users);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => (db_status = 'WE GOT THIS CONNECTION. LETS GOOOOOOOO'));
 
 app.get('/', (req, res) => {
-	res.send(db_status);
+	res.status(200).send(db_status);
 });
 
-app.listen(4000, () => {
-	console.log('Running from the year 4000');
-});
-
-module.exports = db;
+app.listen(2000, () => console.log('listening on 2000 my dude'));
